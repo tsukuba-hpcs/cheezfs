1d93a5e9592e20064a863e64fb79f8ded95d91c2
Sohei Koyama <skoyama@hpcs.cs.tsukuba.ac.jp>
2025-03-26 13:07:43 +0900

    update

--- cheezfs.c
+++ cheezfs.c
@@ -17,6 +17,7 @@
 #include <sys/xattr.h>
 #include <linux/fs.h>
 #include <sys/ioctl.h>
+#include <jansson.h>
 
 #include "tree.h"
 
@@ -61,9 +62,28 @@ static const struct fuse_opt czfs_opts[] = {
 
 RB_GENERATE(czfs_itree, czfs_inode, link, inode_compare);
 
+struct czfs_tensor {
+	char *key;
+	int fd;
+	size_t data_offsets[2];
+	RB_ENTRY(czfs_tensor) link;
+};
+
+static int
+tensor_compare(struct czfs_tensor *a, struct czfs_tensor *b)
+{
+	fprintf(stderr, "compare a=%s b=%s\n", a->key, b->key);
+	return strcmp(a->key, b->key);
+}
+
+RB_HEAD(czfs_ttree, czfs_tensor);
+RB_GENERATE(czfs_ttree, czfs_tensor, link, tensor_compare);
+
 struct czfs_filep {
 	int fd;
 	int state;
+	uint64_t n;
+	struct czfs_ttree tensors;
 };
 
 static struct czfs_filep *
@@ -420,6 +440,17 @@ czfs_readdirplus(fuse_req_t req, fuse_ino_t ino, size_t size, off_t off,
 	do_readdir(req, ino, size, off, fi, 1);
 }
 
+static bool
+has_safetensor_suffix(const char *filename)
+{
+	const char *suffix = ".safetensors";
+	size_t len_filename = strlen(filename);
+	size_t len_suffix = strlen(suffix);
+	if (len_filename < len_suffix)
+		return false;
+	return strcmp(filename + len_filename - len_suffix, suffix) == 0;
+}
+
 static void
 czfs_create(fuse_req_t req, fuse_ino_t parent, const char *name, mode_t mode,
 	    struct fuse_file_info *fi)
@@ -434,9 +465,14 @@ czfs_create(fuse_req_t req, fuse_ino_t parent, const char *name, mode_t mode,
 		fuse_reply_err(req, errno);
 		return;
 	}
-	fh->state = CHEEZ_INIT;
+	if (has_safetensor_suffix(name)) {
+		fh->state = CHEEZ_INIT;
+	} else {
+		fh->state = CHEEZ_IGNORE;
+	}
 
 	fi->fh = (uint64_t)fh;
+	RB_INIT(&fh->tensors);
 
 	err = do_lookup(req, parent, name, &e);
 	if (err) {
@@ -461,6 +497,7 @@ czfs_open(fuse_req_t req, fuse_ino_t ino, struct fuse_file_info *fi)
 	}
 	fh->state = CHEEZ_INIT;
 	fi->fh = (uint64_t)fh;
+	RB_INIT(&fh->tensors);
 
 	if (fi->flags & O_DIRECT)
 		fi->direct_io = 1;
@@ -490,11 +527,176 @@ czfs_read(fuse_req_t req, fuse_ino_t ino, size_t size, off_t offset,
 	fuse_reply_data(req, &buf, FUSE_BUF_SPLICE_MOVE);
 }
 
+static int
+czfs_parse_json(struct fuse_file_info *fi, char *buf)
+{
+	json_error_t error;
+	char *json = malloc(czfs_filep(fi)->n + 1);
+	json[czfs_filep(fi)->n] = '\0';
+	memcpy(json, buf, czfs_filep(fi)->n);
+	fprintf(stderr, "%s\n", json);
+	json_t *root = json_loads(json, 0, &error);
+	void *iter;
+	if (!root) {
+		fprintf(stderr, "error: on line %d: %s\n", error.line,
+			error.text);
+		return 1;
+	}
+	if (!json_is_object(root)) {
+		fprintf(stderr, "error: root is not an object\n");
+		return 1;
+	}
+	iter = json_object_iter(root);
+	while (iter) {
+		const char *key = json_object_iter_key(iter);
+		fprintf(stderr, "key=%s\n", key);
+		char *dupkey = strdup(key);
+		json_t *value = json_object_iter_value(iter);
+		if (strcmp(key, "__metadata__") != 0) {
+			struct czfs_tensor *tensor = malloc(sizeof(*tensor));
+			tensor->key = dupkey;
+			tensor->fd = czfs_filep(fi)->fd;
+			json_t *data_offsets =
+			    json_object_get(value, "data_offsets");
+			if (!json_is_array(data_offsets)) {
+				fprintf(
+				    stderr,
+				    "error: data_offsets is not an array\n");
+				free(tensor);
+				free(dupkey);
+				return 1;
+			}
+			size_t i;
+			for (i = 0; i < json_array_size(data_offsets); i++) {
+				json_t *o = json_array_get(data_offsets, i);
+				if (json_is_integer(o)) {
+					size_t v =
+					    (size_t)json_integer_value(o);
+					tensor->data_offsets[i] = v;
+				} else {
+					fprintf(stderr,
+						"Non-integer value in array at "
+						"index %zu\n",
+						i);
+					free(tensor);
+					free(dupkey);
+					return 1;
+				}
+			}
+			fprintf(stderr, "dump key=%s offset=[%zu, %zu]\n",
+				tensor->key, tensor->data_offsets[0],
+				tensor->data_offsets[1]);
+			RB_INSERT(czfs_ttree, &czfs_filep(fi)->tensors, tensor);
+		} else
+			free(dupkey);
+		iter = json_object_iter_next(root, iter);
+	}
+	return 0;
+}
+
 static void
 czfs_write_buf(fuse_req_t req, fuse_ino_t ino, struct fuse_bufvec *in_buf,
 	       off_t off, struct fuse_file_info *fi)
 {
 	ssize_t res;
+	size_t len = fuse_buf_size(in_buf);
+	fprintf(stderr, "state=%d len=%zu\n", czfs_filep(fi)->state, len);
+	switch (czfs_filep(fi)->state) {
+	case CHEEZ_INIT: {
+		char *buf = malloc(len);
+		struct fuse_bufvec tmp = FUSE_BUFVEC_INIT(len);
+		tmp.buf[0].flags = 0;
+		tmp.buf[0].mem = buf;
+		tmp.buf[0].size = len;
+		tmp.buf[0].pos = 0;
+		res = fuse_buf_copy(&tmp, in_buf, 0);
+		if (res != len) {
+			if (res < 0) {
+				fuse_reply_err(req, -res);
+				free(buf);
+				return;
+			} else {
+				len = res;
+				fprintf(stderr, "ignore res=%zu\n",
+					(size_t)res);
+				czfs_filep(fi)->state = CHEEZ_IGNORE;
+				goto flush_init;
+			}
+		}
+		if (len == 8) {
+			uint64_t n = *(uint64_t *)buf;
+			czfs_filep(fi)->n = le64toh(n);
+			czfs_filep(fi)->state = CHEEZ_PREPARE;
+			goto flush_init;
+		} else if (len > 8) {
+			czfs_filep(fi)->n = le64toh(*(uint64_t *)(buf));
+			if (len == czfs_filep(fi)->n + 8) {
+				char *metadata = buf + 8;
+				if (czfs_parse_json(fi, metadata)) {
+					czfs_filep(fi)->state = CHEEZ_IGNORE;
+					goto flush_init;
+				}
+				czfs_filep(fi)->state = CHEEZ_BULK;
+			} else {
+				czfs_filep(fi)->state = CHEEZ_IGNORE;
+			}
+			goto flush_init;
+		} else {
+			czfs_filep(fi)->state = CHEEZ_IGNORE;
+			goto flush_init;
+		}
+	flush_init:
+		res = pwrite(czfs_filep(fi)->fd, buf, len, off);
+		if (res < 0) {
+			fuse_reply_err(req, -res);
+		} else {
+			fuse_reply_write(req, res);
+		}
+		free(buf);
+		return;
+	}
+	case CHEEZ_PREPARE: {
+		char *buf = malloc(len);
+		struct fuse_bufvec tmp = FUSE_BUFVEC_INIT(len);
+		tmp.buf[0].flags = 0;
+		tmp.buf[0].mem = buf;
+		tmp.buf[0].size = 8;
+		tmp.buf[0].pos = 0;
+		res = fuse_buf_copy(&tmp, in_buf, 0);
+		if (res != len) {
+			if (res < 0) {
+				fuse_reply_err(req, -res);
+				free(buf);
+				return;
+			} else {
+				len = res;
+				czfs_filep(fi)->state = CHEEZ_IGNORE;
+				goto flush_prepare;
+			}
+		}
+
+		if (len != czfs_filep(fi)->n) {
+			czfs_filep(fi)->state = CHEEZ_IGNORE;
+			goto flush_prepare;
+		}
+		if (czfs_parse_json(fi, buf)) {
+			czfs_filep(fi)->state = CHEEZ_IGNORE;
+			goto flush_prepare;
+		}
+		czfs_filep(fi)->state = CHEEZ_BULK;
+	flush_prepare:
+		res = pwrite(czfs_filep(fi)->fd, buf, len, off);
+		if (res < 0) {
+			fuse_reply_err(req, -res);
+		} else {
+			fuse_reply_write(req, res);
+		}
+		free(buf);
+		return;
+	}
+	default:
+		break;
+	}
 	struct fuse_bufvec out_buf = FUSE_BUFVEC_INIT(fuse_buf_size(in_buf));
 
 	out_buf.buf[0].flags = FUSE_BUF_IS_FD | FUSE_BUF_FD_SEEK;
@@ -503,9 +705,11 @@ czfs_write_buf(fuse_req_t req, fuse_ino_t ino, struct fuse_bufvec *in_buf,
 
 	res = fuse_buf_copy(&out_buf, in_buf, 0);
 	if (res < 0) {
+		fprintf(stderr, "fuse_buf_copy res=%s\n", strerror(-res));
 		fuse_reply_err(req, -res);
 		return;
 	}
+	fprintf(stderr, "fuse_reply_write %zu\n", (size_t)res);
 	fuse_reply_write(req, (size_t)res);
 }
 
